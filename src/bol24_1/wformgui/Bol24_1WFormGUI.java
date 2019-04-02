f3f5a8edda5a253181daa4173cf73a39ed5ce1c9
jmartinezpineiro <jmartinezpineiro@nautilus16.danielcastelao.org>
2019-04-02 10:38:53 +0200

    Boletin terminado

---
+++ src/bol24_1/wformgui/Bol24_1WFormGUI.java
@@ -0,0 +1,21 @@
+package bol24_1.wformgui;
+
+/**
+ *
+ * @author jmartinezpineiro
+ */
+public class Bol24_1WFormGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Ventana objventana = new Ventana();
+        objventana.setVisible(true);
+        
+
+
+    }
+
+}
